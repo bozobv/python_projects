a0f8f4cb05fc496dbb30aaf44a0792ddf534c5c9
Bálint Vid Bozó <61810357+bozobv@users.noreply.github.com>
2024-07-28 16:32:05 +0200

    Add files via upload

--- day35/main.py
+++ day35/main.py
@@ -1,24 +1,49 @@
 import requests
+import os
+from twilio.rest import Client
 
 API = ""
 LAT = 47.501423
 LNG = 19.071974
+TEST_LAT = 47.281280
+TEST_LNG = 15.969010
 
-parameters = {
-    "lat" : LAT,
-    "lon" : LNG,
-    "appid" : API,
-    "cnt" : 4
-}
+def ask_12hours_code():
 
-response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast?", params=parameters)
-response.raise_for_status()
-weather_data = response.json()["list"]
+    parameters = {
+        "lat" : TEST_LAT,
+        "lon" : TEST_LNG,
+        "appid" : API,
+        "cnt" : 4
+    }
 
-for data_chunk in weather_data:
-    weather_code = data_chunk["weather"][0]["id"]
-    if weather_code < 700:
-        print("teso, vigyél esernyőt")
-    print(weather_code)
+    response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast?", params=parameters)
+    response.raise_for_status()
+    weather_data = response.json()["list"]
 
-#print(weather_data)
+    will_rain = False
+
+    for data_chunk in weather_data:
+        weather_code = data_chunk["weather"][0]["id"]
+        if weather_code < 700:
+            will_rain = True
+        #print(weather_code)
+    
+    return will_rain
+
+
+def test_send_sms():
+    account_sid = ""
+    auth_token = ""
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body="Ma esni fog az eső, tesó. Vigyél esernyőt!",
+        from_="",
+        to="",
+    )
+
+    print(message.body)
+
+if ask_12hours_code():
+    test_send_sms()
